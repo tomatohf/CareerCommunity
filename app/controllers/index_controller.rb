d9fe243ee63ccd588d50cc70497f348efbf70ddc
Tomato <Tomato.HF@gmail.com>
2008-10-10 14:13:37 +0800

    fix coach page sink problem on IE 6

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,8 +3,8 @@ class IndexController < ApplicationController
   def index
     
     @display_activity_posts = [
-      ["望闻问切 把脉职场 - 成功求职的10个关键！", "/images/real_index/activity_posts/first_session_20081012.jpg", "/activities/1002"],
-      ["望闻问切 把脉职场", "/images/real_index/activity_posts/first_session_20081012.jpg", "/activities/1002"]
+      [" 上海中医药大学讲座", "/images/real_index/activity_posts/zhongyiyao_20081016.jpg", "/activities/1003"],
+      ["望闻问切 把脉职场 - 成功求职的10个关键！", "/images/real_index/activity_posts/first_session_20081012.jpg", "/activities/1002"]
     ]
 
   end
