d6f5fa52acb94ef52554156692714067aa8035b4
Tomato <Tomato.HF@gmail.com>
2008-12-25 16:20:21 +0800

    none

--- public/javascripts/tinymce.js
+++ public/javascripts/tinymce.js
@@ -9,7 +9,7 @@ tinyMCE.init(
 		remove_script_host : false,
 	
 		// Theme options
-		theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,formatselect,fontselect,fontsizeselect,|,justifyleft,justifycenter,justifyright,|,forecolor,backcolor,|,bullist,numlist,|,outdent,indent,blockquote",
+		theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,formatselect,fontsizeselect,|,justifyleft,justifycenter,justifyright,|,forecolor,backcolor,|,bullist,numlist,|,outdent,indent,blockquote",
 		theme_advanced_buttons2 : "pasteword,|,search,|,undo,redo,|,insertdate,inserttime,|,cleanup,removeformat,|,hr,sub,sup,|,charmap,emotions,|,link,unlink,anchor,image,albumphotobtn,|,code,|,fullscreen,|,preview",
 		theme_advanced_buttons3 : "",
 		theme_advanced_toolbar_location : "top",
