2a80dc2b16b60a5881e5ec81c9124990524d288b
Tomato <Tomato.HF@gmail.com>
2009-09-27 17:28:20 +0800

    Upgrade to Rails 2.3.4 day 3

--- config/environment.rb
+++ config/environment.rb
@@ -86,8 +86,4 @@ Rails::Initializer.run do |config|
 
   # Make Active Record use UTC-base instead of local time
   # config.active_record.default_timezone = :utc
-  
-  
-  # config middlewares
-  #config.middleware.use Rack::Deflater
 end
