80befeda639f7109d4f8c6816554b2c99c51cbff
Tomato <Tomato.HF@gmail.com>
2010-06-08 14:27:21 +0800

    none

--- app/models/intranet/employee.rb
+++ app/models/intranet/employee.rb
@@ -5,7 +5,7 @@ module Intranet
       [
         {:id => 10, :name => "何帆", :account_id => 1001, :manager => true, :email => "hefan@"},
         {:id => 20, :name => "马晓", :account_id => 1002, :manager => true, :email => "rick@"},
-        {:id => 30, :name => "沈锴", :account_id => 1019, :manager => false, :email => "shenkai@"},
+        {:id => 30, :name => "沈锴", :account_id => 1019, :manager => true, :email => "shenkai@"},
         
         {:id => 40, :name => "顾艳", :account_id => 1422, :manager => false, :email => "guyan@woshiyanzijie"},
         {:id => 50, :name => "陈松凤", :account_id => 6401, :manager => false, :email => "debbychen@36915105"},
