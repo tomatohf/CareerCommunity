65e815d61b94d64265fc7999bf06e11734c463ec
Tomato <Tomato.HF@gmail.com>
2010-08-13 19:17:18 +0800

    none

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -5,6 +5,20 @@ class TrainingsController < ApplicationController
   end
   
   
+  def cases
+    @cases = [
+      ["expo", "世博志愿者培训"],
+      ["tiyan", "世博志愿者七彩体验营"],
+      ["yili", "伊利世博项目门店销售培训"],
+      ["pudong", "浦东新区团干部骨干培训班"],
+      ["cangyuan", "沧源科技园区中小企业销售培训"]
+    ]
+    
+    @case = @cases.detect{ |c| c[0] == params[:id] }
+    return jump_to("/trainings/cases/expo") unless @case
+  end
+  
+  
   def about
     
   end
