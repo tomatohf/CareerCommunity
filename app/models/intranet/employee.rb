fb5b3d38c0b5e4e39581cd4e3bcbebf25371266c
Tomato <Tomato.HF@gmail.com>
2011-07-31 16:51:36 +0800

    none

--- app/models/intranet/employee.rb
+++ app/models/intranet/employee.rb
@@ -10,10 +10,10 @@ module Intranet
         # {:id => 50, :name => "陈松凤", :account_id => 6401, :manager => false, :email => "debbychen@36915105"},
         # {:id => 70, :name => "王佳俊", :account_id => 6398, :manager => false, :email => "leowang@80525361"},
         {:id => 80, :name => "张杰", :account_id => 1074, :manager => false, :email => "zhangjie@iamzj"},
-        {:id => 90, :name => "刘偲", :account_id => 6616, :manager => false, :email => "liucai@liuc@i"},
+        # {:id => 90, :name => "刘偲", :account_id => 6616, :manager => false, :email => "liucai@liuc@i"},
         {:id => 100, :name => "陆洁扬", :account_id => 6887, :manager => false, :email => "lujieyang@lujieyang1103"},
         
-        {:id => 110, :name => "汪刘益", :account_id => 7114, :manager => false, :email => "wangliuyi@609234989"},
+        # {:id => 110, :name => "汪刘益", :account_id => 7114, :manager => false, :email => "wangliuyi@609234989"},
         {:id => 120, :name => "刘鹤鸣", :account_id => 7292, :manager => false, :email => "liuheming@raulgood"},
         {:id => 130, :name => "陆圆", :account_id => 7317, :manager => false, :email => "luyuan@900103"},
         # {:id => 140, :name => "方旭东", :account_id => , :manager => false, :email => "fangxudong@0874018"},
