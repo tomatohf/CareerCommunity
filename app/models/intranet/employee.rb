a32d2d2f8463aca426ac5f273dada0aa73ccb7c6
Tomato <Tomato.HF@gmail.com>
2013-04-01 10:18:13 +0800

    remove employee

--- app/models/intranet/employee.rb
+++ app/models/intranet/employee.rb
@@ -15,7 +15,7 @@ module Intranet
         
         # {:id => 110, :name => "汪刘益", :account_id => 7114, :manager => false, :email => "wangliuyi@609234989"},
         # {:id => 120, :name => "刘鹤鸣", :account_id => 7292, :manager => false, :email => "liuheming@raulgood"},
-        {:id => 130, :name => "陆圆", :account_id => 7317, :manager => false, :email => "luyuan@900103"},
+        # {:id => 130, :name => "陆圆", :account_id => 7317, :manager => false, :email => "luyuan@900103"},
         # {:id => 140, :name => "方旭东", :account_id => , :manager => false, :email => "fangxudong@0874018"},
         # {:id => 150, :name => "沙宏刚", :account_id => , :manager => false, :email => "shahonggang@xiaosha"},
         # {:id => 160, :name => "刘玥佩", :account_id => , :manager => false, :email => "liuyuepei@04211123"},
